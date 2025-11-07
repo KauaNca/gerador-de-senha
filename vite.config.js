0e6cb2521d1f7ce6dd87ebb12adfd1eb6753571d
Kauã Nunes <kauanunes2003@gmail.com>
2025-11-07 15:11:35 -0300

    finalizado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/gerador-de-senha/", // <-- o nome do seu repositório
   plugins: [
     tailwindcss(),
     react({
